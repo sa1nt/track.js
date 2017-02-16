67f724059705750abbf388acffb77ff2d784dde9
Anatoly Mozgov <sa1nt87@gmail.com>
2017-02-17 01:20:04 +0200

    Separated sending own location to server and getting others locations from server into separate functions

--- app.js
+++ app.js
@@ -30,13 +30,16 @@ var app = connect();
 var userLocations = {};
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use('/location', function(request, response){
-	console.log(request.body);
+app.use('/location/clear', function(request, response){
 	if (request.method === 'GET') {
 		response.writeHead(200, {'Content-Type': 'text/plain'});
 
 		var url_parts = url.parse(request.url, true);
-        console.log('Query: ', url_parts.query);
+        console.log('GET /clear request. Query: ', url_parts.query);
+        // GET to /location/clear&uuid={uuid}
+        //	will delete user with {uuid} from memory
+        // GET to /location/clear 
+    	//	will delete all users from memory
         if (url_parts.query.clear !== undefined) {
         	if (url_parts.query.uuid) {
         		response.write('Deleted user ' + url_parts.query.uuid + '\n');
@@ -50,13 +53,21 @@ app.use('/location', function(request, response){
 		// Send the response body as "Hello World"
 		response.end('OK\n');
 	}
+});
+app.use('/location', function(request, response) {
+	if (request.method === "GET") {
+		var result = Object.assign({}, userLocations);
+		console.log('GET /location request');
+		response.writeHead(200, {
+			"Content-Type": "application/json"
+		});
+		response.end(JSON.stringify(result));
+	}
 	if (request.method === "POST") {
+		console.log('POST /location request. Body: ', request.body);
 		// react only if field 'name' exists in request and isn't empty
 		if (request.body.name && request.body.uuid) {
-			var result = Object.assign({}, userLocations);
 			// if a request contains location info, then update userLocations Map 
-			//	and exclude this user's info from response
-			// otherwise, return location info for all users
 			if(request.body.latitude && request.body.longtitude) { 
 				var userKey = request.body.uuid;
 				userLocations[userKey] = {
@@ -64,14 +75,9 @@ app.use('/location', function(request, response){
 					'latitude': request.body.latitude,
 					'longtitude': request.body.longtitude
 				};
-
-				delete result[userKey];
 			}
-			console.log(result);
-			response.writeHead(200, {
-		      "Content-Type": "application/json"
-			});
-			response.end(JSON.stringify(result));
+			response.writeHead(200, {'Content-Type': 'text/plain'});
+			response.end('OK\n');
 		}
   	}
 });
