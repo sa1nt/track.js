ce2c6e18e5e5c77f26ae82297240bd58a87c4733
Anatoly Mozgov <sa1nt87@gmail.com>
2017-02-20 21:27:38 +0200

    Found reason of app not working in Android Chrome (must use HTTPS to do geolocation). Also, points

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ var http = require('http');
 var app = connect();
 
 var userLocations = {};
+var pointLocations = {};
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
 app.use('/location/clear', function(request, response){
 	if (request.method === 'GET') {
 		response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -57,6 +60,61 @@ app.use('/location', function(request, response) {
 		} else {
 			response.write(400);
 			response.end('POST /location request should be a JSON with "name", "uuid", "latitude" and "longtitude" elements');
+		}
+	}
+});
+
+app.use('/point/clear', function(request, response){
+	if (request.method === 'GET') {
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+
+		var url_parts = url.parse(request.url, true);
+		console.log('GET /point/clear request. Query: ', url_parts.query);
+		// GET to /point/clear?uuid={uuid}
+		//	will delete point with {uuid} from memory
+		// GET to /point/clear
+		//	will delete all points from memory
+		if (url_parts.query.uuid) {
+			response.write('Deleted point ' + url_parts.query.uuid + '\n');
+			pointLocations[url_parts.query.uuid]['latitude'] = -1;
+			pointLocations[url_parts.query.uuid]['longtitude'] = -1;
+//			delete pointLocations[url_parts.query.uuid];
+		} else {
+			response.write('Deleted all ' + Object.keys(pointLocations).length + '\n');
+			for (var pointUuid in pointLocations) {
+				if (pointLocations.hasOwnProperty(pointUuid)) {
+					pointLocations[pointUuid]['latitude'] = -1;
+					pointLocations[pointUuid]['longtitude'] = -1;
+				}
+			}
+		}
+
+		// Send the response body as "Hello World"
+		response.end('OK\n');
+	}
+});
+app.use('/point', function(request, response) {
+	if (request.method === "GET") {
+		response.writeHead(200, {
+			"Content-Type": "application/json"
+		});
+		response.end(JSON.stringify(pointLocations));
+	}
+	if (request.method === "POST") {
+		console.log('POST /point request. Body: ', request.body);
+		if (request.body.uuid && request.body.latitude && request.body.longtitude) {
+			// if a request contains location info, then update userLocations Map 
+			var pointKey = request.body.uuid;
+			pointLocations[pointKey] = {
+//				'name': request.body.name,
+				'latitude': request.body.latitude,
+				'longtitude': request.body.longtitude
+			};
+			response.writeHead(200, {'Content-Type': 'text/plain'});
+			response.end('OK\n');
+		} else {
+			response.write(400);
+			response.end('POST /location request should be a JSON with "uuid", "latitude" and "longtitude" elements');
 		}
 	}
 });
